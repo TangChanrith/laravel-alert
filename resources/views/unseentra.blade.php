d7f8830970992bc55b0a7d3e9ae38bd91e5359bc
Tang Chanrith <tang.chanrith@gmail.com>
2018-09-06 13:05:07 +0700

    fixed unseentr template

--- resources/views/unseentra.blade.php
+++ resources/views/unseentra.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-box alert-{{ session()->get('alert.style') }} alert-dismissible fade in show" role="alert">
     	<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     	@if(session()->has('alert.title'))
-			<h6>session()->get('alert.title')</h6>
+			<h6>{{ session()->get('alert.title') }}</h6>
 		@endif
         {!! session()->get('alert.message') !!}
     </div>
